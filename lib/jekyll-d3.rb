7764e08085b728d46872328b87db5ff6806441df
manunamz <manunamz@pm.me>
2021-08-16 08:29:48 -0400

    fix: fix how links are built and docs retrieved from those links in 'generate_json_net_web'.

--- lib/jekyll-d3.rb
+++ lib/jekyll-d3.rb
@@ -155,18 +155,18 @@ module Jekyll
               label: doc.data['title'],
             }
             # TODO: this link calculation ends up with duplicates -- re-visit this later.
-            all_links = @site.link_index.index[doc.url].attributes + @site.link_index.index[doc.url].forelinks
-            if !all_links.nil?
-              all_links.each do |link| # link = { 'type' => str, 'doc_url' => str }
-                # TODO: Header + Block-level wikilinks
-                                                           # remove baseurl and any anchors
-                linked_doc = @md_docs.select{ |d| d.url == link['doc_url'].gsub(@site.baseurl, "").match(/([^#]+)/i)[0] }
-                if !linked_doc.nil? && linked_doc.size == 1 && !excluded_in_graph?(linked_doc.first.type)
-                  net_web_links << {
-                    source: relative_url(doc.url),
-                    target: relative_url(linked_doc.first.url),
-                  }
-                end
+            all_valid_links = @site.link_index.index[doc.url].attributes + @site.link_index.index[doc.url].forelinks
+            all_valid_links.each do |link| # link = { 'type' => str, 'doc_url' => str }
+              # TODO: Header + Block-level wikilinks
+              link_no_anchor = link['doc_url'].match(/([^#]+)/i)[0]
+              link_no_baseurl = @site.baseurl.nil? ? link_no_anchor : link_no_anchor.gsub(@site.baseurl, "")
+              linked_doc = @md_docs.select{ |d| d.url == link_no_baseurl }
+              if !linked_doc.nil? && linked_doc.size == 1 && !excluded_in_graph?(linked_doc.first.type)
+                # TODO: add link['type'] to d3 graph
+                net_web_links << {
+                  source: relative_url(doc.url),
+                  target: relative_url(linked_doc.first.url),
+                }
               end
             end
 
